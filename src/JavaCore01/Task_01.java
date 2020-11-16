23e438de3761f7ca496834110aeb61deab277803
AlexCherton <Alex_General2002@mail.ru>
2020-11-16 22:34:09 +0300

    Создание репозитория для Java-курса

---
+++ src/JavaCore01/Task_01.java
@@ -0,0 +1,13 @@
+package JavaCore01;
+
+public class Task_01 {
+    public static void main (String[] Args){
+    int i = 0;
+        String s = "JAVA";
+        while (i<10) {
+            System.out.print(s + " ");
+            i = i + 1;
+        }
+    }
+
+}
