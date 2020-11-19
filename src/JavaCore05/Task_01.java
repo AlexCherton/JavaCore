4b5369de345b80de9e61232f62b9df33f0c67cc9
AlexCherton <Alex_General2002@mail.ru>
2020-11-19 21:42:56 +0300

    Выложено решение 1 задание 5 дня

---
+++ src/JavaCore05/Task_01.java
@@ -0,0 +1,59 @@
+package JavaCore05;
+
+public class Task_01 {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setModel("Лада седан");
+        car1.setColor("Баклажан");
+        car1.setYearOfManufactory(2015);
+        String m1 = car1.getModel();
+        String c1 = car1.getColor();
+        int y1 = car1.getYearOfManufactory();
+        System.out.println("Автомобиль модели " +"\""+ m1 +"\""+ " цвета " +"\""+ c1 +"\""+" "+ y1 + " года выпуска.");
+    }
+}
+
+class Car {
+    // у класса могут быть поля и класс может совершать действия через методы
+    private String model;
+    private int yearOfManufactory;
+    private String color;
+
+    public void setModel(String modelName) {
+        if (modelName.isEmpty()) {
+            System.out.println("Выввели пустое наименование модели");
+        } else {
+            model = modelName;
+        }
+    }
+
+    public void setColor(String colorName) {
+        if (colorName.isEmpty()) {
+            System.out.println("Выввели пустое наименование цвета");
+        } else {
+            color = colorName;
+        }
+    }
+
+
+    public void setYearOfManufactory(int manufactoryYear) {
+        if (manufactoryYear < 0) {
+            System.out.println("Выввели отрицательный возраст");
+        } else {
+            yearOfManufactory = manufactoryYear;
+        }
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYearOfManufactory() {
+        return yearOfManufactory;
+    }
+}
+
